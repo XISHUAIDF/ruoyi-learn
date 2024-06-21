6fa7893a396f65d68372896d81c12da0cf44c515
zjh <3020905894@qq.com>
2024-06-21 18:39:05 +0800

    活动参与者登录

---
+++ ruoyi-competition/src/main/java/com/ruoyi/competition/service/imp/IParticipantsLoginService.java
@@ -0,0 +1,7 @@
+package com.ruoyi.competition.service.imp;
+
+public interface IParticipantsLoginService {
+    public String login(String username, String password, String code, String uuid);
+    public void validateCaptcha(String username, String code, String uuid);
+    public void loginPreCheck(String username, String password);
+}
