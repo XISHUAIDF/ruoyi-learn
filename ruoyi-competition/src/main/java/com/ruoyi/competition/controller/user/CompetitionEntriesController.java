01eb072fadb69fe9a347f0cc8d5658b842b698d6
zjh <3020905894@qq.com>
2024-06-22 10:31:57 +0800

    作品提交

--- ruoyi-competition/src/main/java/com/ruoyi/competition/controller/user/CompetitionEntriesController.java
+++ ruoyi-competition/src/main/java/com/ruoyi/competition/controller/user/CompetitionEntriesController.java
@@ -1,29 +1,19 @@
-package com.ruoyi.competition.controller;
+package com.ruoyi.competition.controller.user;
 
 
-import javax.servlet.http.HttpServletResponse;
-
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.competition.domain.CompetitionEntries;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.enums.BusinessType;
-
-import com.ruoyi.competitionEntry.service.ICompetitionEntriesService;
-
 import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.competition.domain.CompetitionEntries;
+import com.ruoyi.competitionEntry.service.ICompetitionEntriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -44,7 +34,7 @@ public class CompetitionEntriesController extends BaseController
      */
     @PreAuthorize("@ss.hasPermi('test:entries:list')")
     @GetMapping("/list")
-    public TableDataInfo list(CompetitionEntries competitionEntries)
+    public TableDataInfo list(@RequestBody CompetitionEntries competitionEntries)
     {
         startPage();
         List<CompetitionEntries> list = competitionEntriesService.selectCompetitionEntriesList(competitionEntries);
